7051b19c1038f8c866c3ac02ed86f00c412ec425
Kaan B. Y. <109346960+6b6279@users.noreply.github.com>
2025-09-15 09:39:09 +0200

    Implement/adapt DoplerStatistics

--- src/main/java/edu/kit/travart/dopler/transformation/util/DoplerStatistics.java
+++ src/main/java/edu/kit/travart/dopler/transformation/util/DoplerStatistics.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Logger;
 
 import at.jku.cps.travart.core.common.IStatistics;
 import edu.kit.dopler.model.Dopler;
+import edu.kit.dopler.model.EnumerationDecision;
 
 public class DoplerStatistics implements IStatistics<Dopler> {
 	
@@ -22,8 +23,14 @@ public class DoplerStatistics implements IStatistics<Dopler> {
 	public int getVariabilityElementsCount(Dopler model) {
 		// TODO Is is the right way to calculate size for DOPLER decision models?
 		// Previously: Only #decisions + #assets
-		return model.getDecisions().stream().mapToInt(e -> e.getRules().size()).sum()
-				+ model.getAssets().size();
+		// Try to calculate the number of branches
+		return model.getDecisions().stream().mapToInt(e -> {
+			if (e instanceof EnumerationDecision) {
+				return ((EnumerationDecision) e).getEnumeration().getEnumerationLiterals().size();
+			} else {
+				return 1;
+			}
+		}).sum() + model.getAssets().size();
 	}
 
 	@Override
